fff7607dee6b7caff3e1c72f12897aec88b3b38f
André Santos <andrefs@cpan.org>
2015-09-22 17:05:08 +0100

    more stuff

--- config.js
+++ config.js
@@ -9,7 +9,9 @@ exports.config = {
         },
         google: {
             id: 'UA-64953131-1'
-        }
+        },
+        author: 'andré santos, david oliveira, joão ferreira',
+        publisher: 'botoes.co'
     },
     partials: {
         footer: '<a href="mailto:andrefs@andrefs.com">andré santos, david oliveira, joão ferreira <i class="fa fa-envelope"></i></a> , baseado em <a href="http://www.facebook.com/brunoaleixo">bruno aleixo<i class="fa fa-external-link"></i></a>'
@@ -23,5 +25,14 @@ exports.config = {
     soundsPath        : './sounds',
     tmpPath           : './tmp',
     defaultClipMargin : '2',
-    soundEditCommand  : 'audacity'
+    soundEditCommand  : 'audacity',
+    toplinks: [
+        {
+            link: 'http://aleixo.botoes.co',
+            title: 'Aleixo'
+        },{
+            link: 'http://beatriz.botoes.co',
+            title: 'Beatriz'
+        }
+    ]
 };
